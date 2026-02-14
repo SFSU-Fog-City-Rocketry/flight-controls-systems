18f2a68b4e0b22d5e8f2264174462fdec78fe890
= <=>
2026-02-13 18:16:49 -0800

    Removed unused files

--- src/drivers/esp32/steady_clock.cpp
+++
@@ -1,9 +0,0 @@
-#include <esp32/steady_clock.hpp>
-#include <cstdint>
-#include "esp_timer.h"
-
-namespace hal::esp32 {
-    uint64_t steady_clock::frequency() {
-        return static_cast<uint64_t>(esp_timer_get_time());
-    }
-};
