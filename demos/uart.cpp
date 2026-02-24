3a378ba25ea1e44f86c080a2306c64ef48671747
= <=>
2026-02-24 13:44:57 -0800

    Minor changes to uart.cpp demo

--- demos/uart.cpp
+++ demos/uart.cpp
@@ -7,9 +7,9 @@
 extern "C" void app_main() {
     hal::esp32::print("Serial", "Demo Application Initiated!");
 
-    std::array<uint8_t, 2048> receive_buffer{};
+    std::array<uint8_t, 2048> bytes{};
     hal::esp32::settings settings{.baud_rate=115200, .tx=0x17, .rx=0x16,};
-    hal::esp32::uart uart_console(UART_NUM_1, receive_buffer, settings);
+    hal::esp32::uart uart_console(UART_NUM_1, bytes, settings);
 
     // Configure TX signal loop back to RX module, just for the test usage. 
     uart_console.set_loop_back(true);
